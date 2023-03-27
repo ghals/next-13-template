d05fc0488eeef1fe5726ec1cbb9cb18e676a2ea3
ghals <hm.kim@socialinvestinglab.com>
2023-03-27 19:53:49 +0900

    feat: 라이브러리 설치

--- components/TestCmp.tsx
+++ components/TestCmp.tsx
@@ -3,3 +3,5 @@ import React from 'react'
 const TestCmp = () => <div>test3333</div>
 
 export default TestCmp
+
+
---
+++ providers/ReactQueryProvider.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React, { ReactNode, useState } from 'react'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
+  const [client] = useState(new QueryClient())
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>
+}
+export default ReactQueryProvider
